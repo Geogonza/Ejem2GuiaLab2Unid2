faa9f4db545c5e30a2e0e064e4e717055531f26f
Usuario <Usuario@DESKTOP-317839F>
2023-09-02 16:24:38 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Ejem2GuiaLab2Unid2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.txtLadoa = new System.Windows.Forms.TextBox();
+            this.txtLadob = new System.Windows.Forms.TextBox();
+            this.txtLadoc = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblTriangulo = new System.Windows.Forms.Label();
+            this.txtTriangulo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(58, 58);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(70, 19);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Lado A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblB.Location = new System.Drawing.Point(61, 114);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(68, 19);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Lado B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC.Location = new System.Drawing.Point(64, 175);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(69, 19);
+            this.lblC.TabIndex = 2;
+            this.lblC.Text = "Lado C";
+            // 
+            // txtLadoa
+            // 
+            this.txtLadoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtLadoa.Location = new System.Drawing.Point(246, 58);
+            this.txtLadoa.Name = "txtLadoa";
+            this.txtLadoa.Size = new System.Drawing.Size(135, 22);
+            this.txtLadoa.TabIndex = 3;
+            // 
+            // txtLadob
+            // 
+            this.txtLadob.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtLadob.Location = new System.Drawing.Point(246, 108);
+            this.txtLadob.Name = "txtLadob";
+            this.txtLadob.Size = new System.Drawing.Size(135, 22);
+            this.txtLadob.TabIndex = 4;
+            // 
+            // txtLadoc
+            // 
+            this.txtLadoc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtLadoc.Location = new System.Drawing.Point(246, 169);
+            this.txtLadoc.Name = "txtLadoc";
+            this.txtLadoc.Size = new System.Drawing.Size(135, 22);
+            this.txtLadoc.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalcular.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(50, 314);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(123, 38);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpiar.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(225, 314);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(101, 38);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalir.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(364, 315);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(98, 39);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblTriangulo
+            // 
+            this.lblTriangulo.AutoSize = true;
+            this.lblTriangulo.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTriangulo.Location = new System.Drawing.Point(68, 240);
+            this.lblTriangulo.Name = "lblTriangulo";
+            this.lblTriangulo.Size = new System.Drawing.Size(90, 19);
+            this.lblTriangulo.TabIndex = 9;
+            this.lblTriangulo.Text = "Triángulo";
+            // 
+            // txtTriangulo
+            // 
+            this.txtTriangulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtTriangulo.Location = new System.Drawing.Point(246, 234);
+            this.txtTriangulo.Name = "txtTriangulo";
+            this.txtTriangulo.Size = new System.Drawing.Size(135, 22);
+            this.txtTriangulo.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(604, 450);
+            this.Controls.Add(this.txtTriangulo);
+            this.Controls.Add(this.lblTriangulo);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtLadoc);
+            this.Controls.Add(this.txtLadob);
+            this.Controls.Add(this.txtLadoa);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Determinar si un triángulo es equilatero ingresando las medidas de sus tres lados" +
+    "";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.TextBox txtLadoa;
+        private System.Windows.Forms.TextBox txtLadob;
+        private System.Windows.Forms.TextBox txtLadoc;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblTriangulo;
+        private System.Windows.Forms.TextBox txtTriangulo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejem2GuiaLab2Unid2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int LadoA, LadoB, LadoC;
+            LadoA = Int32.Parse(txtLadoa.Text);
+            LadoB = Int32.Parse(txtLadob.Text);
+            LadoC = Int32.Parse(txtLadoc.Text);
+
+            if (LadoA == LadoB && LadoA == LadoC)
+            {
+                txtTriangulo.Text = "Equilatero";
+                MessageBox.Show("El triángulo es equilatero");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtLadoa.Text = " ";
+            txtLadob.Text = " ";
+            txtLadoc.Text = " ";
+            txtTriangulo.Text = " ";
+            txtTriangulo.Focus();
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
